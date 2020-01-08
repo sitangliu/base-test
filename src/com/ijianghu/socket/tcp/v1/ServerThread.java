8202e77052f6730d233405726aafc6960f2fe8d1
kai <2459615989@qq.com>
2020-01-08 18:21:13 +0800

    tcp socket 代码提交

--- src/com/ijianghu/socket/tcp/v1/ServerThread.java
+++ src/com/ijianghu/socket/tcp/v1/ServerThread.java
@@ -1,41 +1,41 @@
-//package com.ijianghu.socket.tcp.v1;
-//
-//import java.io.*;
-//import java.net.Socket;
-//
-///**
-// * @author kai
-// * @date 2020/1/7
-// * @desc base-test
-// **/
-//public class ServerThread implements Runnable {
-//    private Socket client;
-//    public ServerThread(Socket client){
-//        this.client = client;
-//    }
-//
-//    @Override
-//    public void run() {
-//        try{
-//            InputStream inputStream = client.getInputStream();
-//            BufferedReader buf = new BufferedReader(new InputStreamReader(inputStream));
-//            OutputStream out = client.getOutputStream();
-//            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
-//            boolean flag = true;
-//            while (flag){
-//                String readLine = buf.readLine();
-//                System.out.println(client.getInetAddress().getHostAddress()+":"+client.getPort()+"  "+readLine);
-//                if(readLine.equals("bye")){
-//                    flag = false;
-//                }else{
-//                    writer.write("hello"+readLine);
-//                    writer.flush();
-//                }
-//            }
-//
-//        }catch (Exception e){
-//            System.err.println(e.getMessage());
-//        }
-//
-//    }
-//}
+package com.ijianghu.socket.tcp.v1;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * @author kai
+ * @date 2020/1/7
+ * @desc base-test
+ **/
+public class ServerThread implements Runnable {
+    private Socket client;
+    public ServerThread(Socket client){
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try{
+            InputStream inputStream = client.getInputStream();
+            BufferedReader buf = new BufferedReader(new InputStreamReader(inputStream));
+            OutputStream out = client.getOutputStream();
+            PrintWriter  writer = new PrintWriter (new OutputStreamWriter(out));
+            boolean flag = true;
+            while (flag){
+                String readLine = buf.readLine();
+                System.out.println(client.getInetAddress().getHostAddress()+":"+client.getPort()+"  "+readLine);
+                if(readLine.equals("bye")){
+                    flag = false;
+                }else{
+                    writer.write("hello"+readLine);
+                    writer.flush();
+                }
+            }
+
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
--- src/com/ijianghu/socket/tcp/v1/SocketServer.java
+++ src/com/ijianghu/socket/tcp/v1/SocketServer.java
@@ -1,34 +1,34 @@
-//package com.ijianghu.socket.tcp.v1;
-//
-//import java.io.*;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//
-///**
-// * @author kai
-// * @date 2020/1/6
-// * @desc base-test
-// **/
-//public class SocketServer {
-//
-//    public static void main(String[] args) {
-//        try{
-//
-//            ServerSocket serverSocket = new ServerSocket(9999);
-//            //设置监听端口
-//            Socket client = null;
-//            boolean f = true;
-//            while(f){
-//                client = serverSocket.accept();
-//                System.out.println("与"+client.getInetAddress().getHostAddress()+":"+client.getPort()+" 已建立连接");
-//                new Thread(new ServerThread(client)).start();
-//            }
-//
-//        }catch (Exception e){
-//            System.out.println(e.getMessage());
-//        }
-//
-//
-//    }
-//
-//}
+package com.ijianghu.socket.tcp.v1;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author kai
+ * @date 2020/1/6
+ * @desc base-test
+ **/
+public class SocketServer {
+
+    public static void main(String[] args) {
+        try{
+
+            ServerSocket serverSocket = new ServerSocket(9999);
+            //设置监听端口
+            Socket client = null;
+            boolean f = true;
+            while(f){
+                client = serverSocket.accept();
+                System.out.println("与"+client.getInetAddress().getHostAddress()+":"+client.getPort()+" 已建立连接");
+                new Thread(new ServerThread(client)).start();
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+}
